0e9a596d3e0f25f8765ddba337c743206c6a2690
Aaryan Potdar <aaruvpotdar@gmail.com>
2023-12-23 22:37:19 -0500

    constants and entity config

---
+++ cellSegmentation/constant/training_pipeline/__init__.py
@@ -0,0 +1,15 @@
+ARTIFACTS_DIR: str = "artifacts"
+
+'''
+Data Ingestion related  constants start with DATA_INGESTION VAR NAME
+'''
+
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+
+DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1ydJ2orU0v-odMwbBMooYS-kBB7ST6i-S/view?usp=drive_link"
+
+
+# artifacts/data_ingestion/data.zip 
+# artifacts/data_ingestion/feature_store/all_features
